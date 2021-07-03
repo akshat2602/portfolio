5418c257df93f941a370c2696c9e668dc7f3be29
Akshat Sharma <akshatsharma2602@gmail.com>
2021-07-03 20:59:26 +0530

    Completed skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,20 @@
 import { Layout, Row, Col, Typography, Card } from 'antd';
-import { DjangoIcon, ReactIcon } from '../../Assets/Svg';
+import {
+    DjangoIcon,
+    ReactIcon,
+    FlaskIcon,
+    TensorflowIcon,
+    PytorchIcon,
+    PythonIcon,
+    JSIcon,
+    CppIcon,
+    DockerIcon,
+    AWSIcon,
+    GitIcon,
+    DynamoIcon,
+    PostgreSQLIcon,
+    MySQLIcon
+} from '../../Assets/Svg';
 
 const { Content } = Layout;
 const { Title } = Typography;
@@ -22,57 +37,69 @@ export const Skills = () => {
                             <Col xxl={5} xl={5} lg={3} xs={2} md={3} sm={3} />
                         </Row>
                         <Row style={{ marginTop: '10vh' }}>
-                            <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={3} />
-                            <Col xxl={18} xl={18} lg={18} xs={20} md={18} sm={18} style={{ textAlign: "left" }}>
-                                <Row>
-                                    <Col xxl={2} xl={2} lg={2} xs={2} md={2} sm={2} />
-                                    <Col xxl={9} xl={9} lg={9} xs={9} md={9} sm={9}>
-                                        <Card data-aos="zoom-out"
-                                            data-aos-delay="200"
-                                            data-aos-once hoverable style={{ borderWidth: 1, borderRadius: 12, width: '100%', margin: 'auto' }}>
-                                            <Title style={{ textAlign: 'center' }} level={3}>Web Frameworks</Title>
-                                            <br />
-                                            <DjangoIcon />
-                                            <ReactIcon />
-                                        </Card>
-                                    </Col>
-                                    <Col xxl={2} xl={2} lg={2} xs={2} md={2} sm={2} />
-                                    <Col xxl={9} xl={9} lg={9} xs={9} md={9} sm={9}>
-                                        <Card data-aos="zoom-out"
-                                            data-aos-delay="200"
-                                            data-aos-once
-                                            hoverable style={{ borderWidth: 1, borderRadius: 12, width: '100%', margin: 'auto' }}>
-                                            <Title style={{ textAlign: 'center' }} level={3}>Languages</Title>
-                                            <br />
-                                        </Card>
-                                    </Col>
-                                    <Col xxl={2} xl={2} lg={2} xs={2} md={2} sm={2} />
-                                </Row>
-                                <br />
-                                <Row>
-                                    <Col xxl={2} xl={2} lg={2} xs={2} md={2} sm={2} />
-                                    <Col xxl={9} xl={9} lg={9} xs={9} md={9} sm={9}>
-                                        <Card data-aos="zoom-out"
-                                            data-aos-delay="200"
-                                            data-aos-once
-                                            hoverable style={{ borderWidth: 1, borderRadius: 12, width: '100%' }}>
-                                            <Title style={{ textAlign: 'center' }} level={3}>Deployment</Title>
-                                            <br />
-                                        </Card>
-                                    </Col>
-                                    <Col xxl={2} xl={2} lg={2} xs={2} md={2} sm={2} />
-                                    <Col xxl={9} xl={9} lg={9} xs={9} md={9} sm={9}>
-                                        <Card data-aos="zoom-out"
-                                            data-aos-delay="200"
-                                            data-aos-once
-                                            hoverable style={{ borderWidth: 1, borderRadius: 12, width: '100%', margin: 'auto' }}>
-                                            <Title style={{ textAlign: 'center' }} level={3}>Databases</Title>
-                                            <br />
-                                        </Card>
-                                    </Col>
-                                    <Col xxl={2} xl={2} lg={2} xs={2} md={2} sm={2} />
-
-                                </Row>
+                            <Col xxl={3} xl={3} lg={4} xs={1} md={3} sm={3} />
+                            <Col xxl={9} xl={9} lg={8} xs={22} md={18} sm={18}>
+                                <Card data-aos="zoom-out"
+                                    data-aos-delay="200"
+                                    data-aos-once
+                                    hoverable
+                                    className="skill-card"
+                                    style={{ borderWidth: 0, borderRadius: 12, marginTop: '5%' }}>
+                                    <Title style={{ textAlign: 'center' }} level={3}>Frameworks/Libraries</Title>
+                                    <br />
+                                    <DjangoIcon />
+                                    <ReactIcon />
+                                    <FlaskIcon />
+                                    <TensorflowIcon />
+                                    <PytorchIcon />
+                                </Card>
+                            </Col>
+                            <Col xxl={0} xl={0} lg={0} xs={1} md={3} sm={3} />
+                            <Col xxl={0} xl={0} lg={0} xs={1} md={3} sm={3} />
+                            <Col xxl={9} xl={9} lg={8} xs={22} md={18} sm={18}>
+                                <Card data-aos="zoom-out"
+                                    data-aos-delay="200"
+                                    data-aos-once
+                                    hoverable
+                                    className="skill-card"
+                                    style={{ borderWidth: 0, borderRadius: 12, marginTop: '5%' }}>
+                                    <Title style={{ textAlign: 'center' }} level={3}>Languages</Title>
+                                    <br />
+                                    <PythonIcon />
+                                    <JSIcon />
+                                    <CppIcon />
+                                </Card>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xxl={3} xl={3} lg={4} xs={1} md={3} sm={3} />
+                            <Col xxl={9} xl={9} lg={8} xs={22} md={18} sm={18}>
+                                <Card data-aos="zoom-out"
+                                    data-aos-delay="200"
+                                    data-aos-once
+                                    className="skill-card"
+                                    hoverable style={{ borderWidth: 0, borderRadius: 12, marginTop: '5%' }}>
+                                    <Title style={{ textAlign: 'center' }} level={3}>Deployment</Title>
+                                    <br />
+                                    <DockerIcon />
+                                    <AWSIcon />
+                                    <GitIcon />
+                                </Card>
+                            </Col>
+                            <Col xxl={0} xl={0} lg={0} xs={1} md={3} sm={3} />
+                            <Col xxl={0} xl={0} lg={0} xs={1} md={3} sm={3} />
+                            <Col xxl={9} xl={9} lg={8} xs={22} md={18} sm={18}>
+                                <Card data-aos="zoom-out"
+                                    data-aos-delay="200"
+                                    data-aos-once
+                                    className="skill-card"
+                                    hoverable style={{ borderWidth: 0, borderRadius: 12, marginTop: '5%' }}>
+                                    <Title style={{ textAlign: 'center' }} level={3}>Databases</Title>
+                                    <br />
+                                    <DynamoIcon />
+                                    <PostgreSQLIcon />
+                                    <MySQLIcon />
+                                </Card>
                             </Col>
                             <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={3} />
                         </Row>
